8768584000fc433685a2b3be1da9948029cbe540
Eyal Mordechai <eyalmordechai@gmail.com>
2026-09-05 10:42:59 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const {Client} = require('pg');
+
+const client = new Client({
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+});
+
+async function start() {
+	await client.connect();
+	console.log('Connected to PostgreSQL');
+	const server = http.createServer(async (req, res) => {
+		console.log(req.method, req.url);
+		const result = await client.query('SELECT * FROM test_data');
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(result.rows));
+	});
+server.listen(3000, '0.0.0.0');
+}
+start();
